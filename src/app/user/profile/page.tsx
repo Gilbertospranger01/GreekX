8611c836147936529593ea72ca18371566c4817e
Gilbertospranger01 <malvespranger2004@gmail.com>
2025-04-01 19:22:19 +0100

    GreekX

--- src/app/user/profile/page.tsx
+++ src/app/user/profile/page.tsx
@@ -70,7 +70,7 @@ const Profile = () => {
         };
 
         fetchUserData();
-    }, []);
+    }, [loading]);
 
     if (!session) {
         return <div className='min-h-screen flex items-center justify-center bg-gray-950'><p className='text-white text-2xl animate-pulse'>Carregando...</p></div>;
